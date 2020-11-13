708d82dbec598c8ca2a84a52d90592c18ea4f2f3
ivoshet <ivan.v.shet@gmail.com>
2020-11-13 23:12:26 +0500

    make migrate

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,3 +1,7 @@
+from django.http import request
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('list of ads will be displayed here')
