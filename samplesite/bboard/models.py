fdd5fed341ab847c5ad97d100eec26d822ad7edb
ivoshet <ivan.v.shet@gmail.com>
2020-11-15 00:18:46 +0500

    added rubrics

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -5,8 +5,20 @@ class Bb(models.Model):
     content = models.TextField(null=True, blank=True, verbose_name='description')
     price = models.FloatField(null=True, blank=True, verbose_name='price')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='published')
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Rubric')
 
     class Meta:
         verbose_name = 'Annoncement'
         verbose_name_plural = 'Annoncement'
         ordering = ['-published']
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='name')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Rubrics'
+        verbose_name = 'Rubric'
+        ordering = ['name']
